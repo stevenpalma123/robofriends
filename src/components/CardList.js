ef87560505a2023d4a55b70a53634bdd34d6a8f7
steven Palma <stevenpalma123@gmail.com>
2020-12-23 21:48:06 -0600

    Finalized RoboFriends App Working version

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -2,9 +2,6 @@ import React from 'react';
 import Card from './Card';
 
 const CardList = ({ robots })=> {
-    if(1){
-        throw new Error('Noooooo')
-    }
     const cardComponent = robots.map((robot)=>{
         return  <Card key={robot.id} id={robot.id} name={robot.name} email={robot.email}/>
     });
